846a4d28ff40e9c00eb3e2aa006a9e7cef5e1971
AthulKrishna14310 <athul.krishna14310@gmail.com>
2020-10-11 12:30:18 +0530

    start

---
+++ start.py
@@ -0,0 +1,21 @@
+#start
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#String variables
+x = "John"
+# is the same as
+x = 'John'
+
+#Multiple assignment
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#Concatenation
+x = "awesome"
+print("Python is " + x)
+
